c6af90f218488c8a2bc5cce75b915e4cab6c2966
Noy <noyhillel@gmail.com>
2015-11-21 03:03:24 +0000

    Changed the comment, didn't read through..

--- src/main/java/me/noyhillel/snakegame/game/SnakeBlocks.java
+++ src/main/java/me/noyhillel/snakegame/game/SnakeBlocks.java
@@ -11,7 +11,7 @@ public final class SnakeBlocks {
 
     /**
      * The reason I do not have to create a constructor, getters and setters is because of the Lombok dependency.
-     * It's a lot easier to create data classes like this using lombok because we can actually determine that this is a data class instead of having border plate code
+     * It's a lot easier to create data classes like this using lombok because we can actually determine that this is a data class instead of having boilerplate code
      * E.g.
      *
      *  public Integer getBlockX() {
@@ -25,6 +25,8 @@ public final class SnakeBlocks {
      *  }
      *
      * These Getters and Setters are automatically created by our 'Data' annotation :D
+     *
+     * It also generates and equals(), hashCode(), and a toString() method.
      *
      * Same concept goes for all other methods.
      *
